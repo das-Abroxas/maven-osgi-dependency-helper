8dd9739d9283644e746c0a6dcedf1a85924091c5
Jannis Hochmuth <jannis.hochmuth@computational.bio.uni-giessen.de>
2020-10-20 09:14:24 +0200

    Add initial script to project

---
+++ dependencies.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------- #
+# ----- Load config and helper variables ---------------------------- #
+# ------------------------------------------------------------------- #
+. ./dependencies.config
+
+# Regex pattern for maven dependency strings
+#  Should be at least <Group ID>:<Artifact ID>:<Version>:provided:<MD5 Hash> 
+#  but can contain more fields before the MD5 hash at the end
+pattern='^([a-zA-Z0-9_\-\.]+:){3,}provided:[a-z0-9]{32}$'
+
+# ------------------------------------------------------------------- #
+# ----- Helper functions -------------------------------------------- #
+# ------------------------------------------------------------------- #
+showHelp() {
+    echo -e "\n  ----- $(basename $0) -----"
+    echo "    A script to determine the transitive dependencies of a maven project "
+    echo "    and check which of them are already deployed to your Liferay instance."
+    echo
+    echo "  Syntax: ./$(basename $0) show   /path/to/maven-project/1 /path/to/maven-project/2 ..."
+    echo "          ./$(basename $0) add    Maven:Dependency:String:1.0.0:MD5Sum Maven:Dependency:String:2.0.0:MD5Sum ..."
+    echo "          ./$(basename $0) remove Maven:Dependency:String:1.0.0:MD5Sum Maven:Dependency:String:2.0.0:MD5Sum ..."
+    echo
+    echo -e "  Help:   ./$(basename $0) -h\n"
+}
+
+# Checks if the first argument of this function is an existing local directory 
+# and if the directory contains a pom.xml file on the first level.
+#
+# Returns: 1 if not a valid directory or pom.xml is missing, 0 else
+isValidMavenDirectory() {
+    path=$1
+
+    if [[ ! -d "$path" ]]; then
+        echo "${bold}${red}$path is no valid path to a directory and will be ignored.${normal}"
+        return 1
+    fi
+    
+    # Look for pom.xml file in the given directory
+    pom=$(find ${path} -maxdepth 1 -type f -name "pom.xml" | wc -l)
+
+    if [[ ${pom} == 0 ]]; then
+        echo "${bold}${red}$path is not a Maven project because it does not contain a pom.xml file. Path will be ignored.${normal}"
+        return 1
+    fi
+}
+
+# Checks if the first argument of this function matches the pattern of a Maven dependency string.
+#
+# Returns: 0 if string matches, 1 else
+isValidMavenDependency() {
+    dependency=$1
+
+    # Check if dependency String matches maven dependency string pattern
+    if [[ ! ${dependency} =~ $pattern ]]; then
+        echo "${red}${dependency} does not maven match dependency string format. Will be ignored.${normal}"
+        return 1
+    fi
+}
+
+showDependencies() {
+    path=$1
+
+    # Project name as last part of the path
+    project=$(basename ${path})
+
+    # List all transitive dependencies with Maven that are declared as provided
+    (cd ${path} && mvn -q dependency:list -DoutputFile=/tmp/dep-list-${project} -DincludeScope=provided)
+    (cd ${path} && mvn -q dependency:tree -DoutputFile=/tmp/dep-tree-${project} -DincludeScope=provided)
+
+    # Remove all lines from list output file that are not dependencies
+    sed -i '/^   /!d' /tmp/dep-list-${project}
+    # Trim whitespaces at start and end of line
+    sed -i 's/^ *//;s/ *$//' /tmp/dep-list-${project}
+
+    # Add MD5-Sum to all dependencies and check against already deployed dependencies
+    while read line ; do
+        md5=$(echo -n $line | md5sum | cut -f 1 -d ' ')
+        sed -i "/$line/s/$/:${md5}/" /tmp/dep-list-${project}
+        sed -i "/$line/s/$/:${md5}/" /tmp/dep-tree-${project}
+
+        if grep -Fq "$md5" ${local_file_path}
+        then
+            sed -i "/$line/s/$/${included}/" /tmp/dep-list-${project}
+            sed -i "/$line/s/$/${included}/" /tmp/dep-tree-${project}
+        fi
+    done < /tmp/dep-list-${project}
+
+    # Print list and tree in terminal
+    echo -e "\n    ${smul}${bold}All needed dependencies of project: ${project}${normal}\n"
+
+    awk -F":" 'BEGIN{ print "\033[1mGroup-ID:Artifact-ID:Type:Version:Scope:MD5-Sum\033[0m\n" } 
+                    $4 ~ /sources/ { next }
+                    { print $1 ":" $2 ":" $3 ":" $4 ":" $5 ":" $6 | "sort" }' /tmp/dep-list-${project} | column -t -s ":"
+
+    echo -e "\n\n    ${smul}${bold}Dependencies that still have to be deployed:${normal}\n"
+
+    grep -v "Already deployed" /tmp/dep-tree-${project} | grep -v ":sources:"
+}
+
+# Copies the remote file with the deployed dependencies to a local directory 
+#  which is specified in the config file of this script.
+#
+# Exits the script with error code 1 if the file with the deployed dependencies cannot be fetched.
+fetchDeployedDependencies() {
+    echo -n "Loading already deployed dependencies ..."
+    scp -q -i ${HOME}/.ssh/${ssh_key} ${ssh_user}@${ssh_host}:${remote_file_path} ${local_file_path}
+
+    if [[ $? -ne 0 ]]; then
+        echo -e " ${bold}${red}Fetching of deployed dependencies failed.${normal}"
+        exit 1
+    else 
+        echo -e " ${green}${bold}Done${normal}"
+    fi
+}
+
+
+# ------------------------------------------------------------------- #
+# ----- Argument parsing -------------------------------------------- #
+# ------------------------------------------------------------------- #
+# Dump all script arguments in an array
+args=("$@")
+# Pop first argument as script command
+cmd=${args[0]}
+# Remove first element from argument array
+args=("${args[@]:1}")
+# Check if script command is valid
+allowed_cmds=("show add remove")
+if [[ "${cmd}" == "-h" ]]; then
+    showHelp
+    exit 0
+elif [[ ! "${allowed_cmds[@]}" =~ "${cmd}" ]]; then
+    echo -e "${red}Error: ${cmd} is no valid execution command of the script!${normal}"
+    showHelp
+    exit 1
+fi
+
+
+# ------------------------------------------------------------------- #
+# ----- Main flow of the script ------------------------------------- #
+# ------------------------------------------------------------------- #
+if [[ ${cmd} == show ]]; then 
+
+    # Download/Update list with already deployed dependencies
+    fetchDeployedDependencies
+
+    for path in "${args[@]}"
+    do
+        # Check if path is a valid directory as well as an Maven project
+        isValidMavenDirectory ${path}
+        [[ $? == 1 ]] && continue
+
+        # Show transitive dependencies of Maven project 
+        showDependencies ${path}
+    done
+
+    #showDependencies "${args[@]}"
+    exit 0
+
+elif [[ ${cmd} == add ]]; then 
+
+    # Append dependency string to file on server
+    for dependency in "${args[@]}"
+    do
+        isValidMavenDependency ${dependency}
+        [[ $? == 1 ]] && continue
+
+        # Append dependency string to list on server
+        ssh -i ${HOME}/.ssh/${ssh_key} ${ssh_user}@${ssh_host} "echo ${dependency} >> ${remote_file_path}"
+    done
+
+elif [[ ${cmd} == remove ]]; then 
+    
+    # Remove dependency strings from file on server
+    for dependency in "${args[@]}"
+    do
+        isValidMavenDependency ${dependency}
+        [[ $? == 1 ]] && continue
+
+        # Remove dependency string from list on server
+        ssh -i ${HOME}/.ssh/${ssh_key} ${ssh_user}@${ssh_host} "sed -i '/^${dependency}$/d' ${remote_file_path}"
+    done
+fi
+
+
